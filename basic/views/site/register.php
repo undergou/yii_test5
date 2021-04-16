26976ef1291914410935e463914846e91f97098f
AliakseiBarysau <undergou@mail.ru>
2021-04-16 23:31:17 +0300

    third

--- basic/views/site/register.php
+++ basic/views/site/register.php
@@ -10,14 +10,14 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'register';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div id="register">
+<div id="block-register">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to Register:</p>
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-register']); ?>
+            <?php $form = ActiveForm::begin(['id' => 'register']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
